171a711a5772ed97e54f6433c1b228530bd1302c
dima475 <dmitroslusarcuk@gmail.com>
2023-07-24 17:16:15 +0300

    Fix code.

---
+++ src/column_names.py
@@ -0,0 +1,49 @@
+# Column names for "Business" DataFrame
+
+business_id = "business_id"
+name = "name"
+address = "address"
+city = "city"
+state = "state"
+postal_code = "postal_code"
+latitude = "latitude"
+longitude = "longitude"
+stars = "stars"
+review_count = "review_count"
+is_open = "is_open"
+attributes = "attributes"
+categories = "categories"
+hours = "hours"
+
+# Column names for "Review" DataFrame
+
+review_id = "review_id"
+user_id = "user_id"
+useful = "useful"
+funny = "funny"
+cool = "cool"
+text = "text"
+date = "date"
+
+# Column names for "User" DataFrame
+
+yelping_since = "yelping_since"
+elite = "elite"
+friends = "friends"
+fans = "fans"
+average_stars = "average_stars"
+compliment_hot = "compliment_hot"
+compliment_more = "compliment_more"
+compliment_profile = "compliment_profile"
+compliment_cute = "compliment_cute"
+compliment_list = "compliment_list"
+compliment_note = "compliment_note"
+compliment_plain = "compliment_plain"
+compliment_cool = "compliment_cool"
+compliment_funny = "compliment_funny"
+compliment_writer = "compliment_writer"
+compliment_photos = "compliment_photos"
+
+# Column names for "Tip" DataFrame
+
+compliment_count = "compliment_count"
